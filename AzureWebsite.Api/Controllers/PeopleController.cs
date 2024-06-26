859d7a4b8e73acbf93d327967d7a95f9d96e1fff
SpazmbiteBackup <spazmbite2@gmail.com>
2024-06-26 22:53:20 +0200

    Clear some codes

--- AzureWebsite.Api/Controllers/PeopleController.cs
+++ AzureWebsite.Api/Controllers/PeopleController.cs
@@ -20,14 +20,7 @@ public class PeopleController : ControllerBase
     [HttpGet(Name = "GetPeople")]
     public async Task<IEnumerable<Person>> Get()
     {
-         /*var mock = new List<Person>
-         {
-             new Person(firstName: "Janek", lastName: "Nowak", phoneNumber: "123321123"),
-             new Person(firstName: "Marek", lastName: "Kowalski")
-         };*/
-
         var people = await db.People.ToListAsync();
         return people;
-        //return mock;
     }
 }
